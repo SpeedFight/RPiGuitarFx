306ca880f91c94de0a7c9345d43f6c30e33a03b5
SF <speedfight_2@wp.pl>
2018-06-05 19:48:08 +0200

    delete seetings that not change anything

--- src/fx/clipper.cpp
+++ src/fx/clipper.cpp
@@ -11,11 +11,11 @@ const std::string Clipper::nameFx("Clipper");
 
 void Clipper::process(jack_nframes_t nframes, JackCpp::AudioIO::audioBufVector inBufs, JackCpp::AudioIO::audioBufVector outBufs){
 
+	static float th = 0.3333333333333;
+
 	float gain = settings.at(0).getValue();
-	float threshold = settings.at(1).getValue()/10.0;
-	float Q = settings.at(2).getValue()/10.0;
-	float character = settings.at(3).getValue()/10.0;
-	float mix = settings.at(4).getValue()/20.0;
+	float Q = settings.at(1).getValue()/10.0;
+	float mix = settings.at(2).getValue()/20.0;
 
 		auto upsampledInput = upSampler->up(inBufs);
 
@@ -23,15 +23,15 @@ void Clipper::process(jack_nframes_t nframes, JackCpp::AudioIO::audioBufVector i
 			upsampledInput[0][i] = (upsampledInput[0][i] + Q) * gain;
 
 			float absVal = std::abs(upsampledInput[0][i]);
-			if (absVal < threshold){
+			if (absVal < th){
 				upsampledInput[0][i] = 2.0 * upsampledInput[0][i];
-			} else if (absVal >= threshold){
+			} else if (absVal >= th){
 				if(upsampledInput[0][i] > 0){
 					upsampledInput[0][i] = (3.0-powf( 2.0-upsampledInput[0][i]*3.0 , 2.0))/3.0;
 					}else{
-						upsampledInput[0][i] = -(3.0-powf(2.0-absVal*3, 2.0))/character;
+						upsampledInput[0][i] = -(3.0-powf(2.0-absVal*3, 2.0))/3.0;
 					}
-			} else if (absVal > 2 * threshold){
+			} else if (absVal > 2 * th){
 				if(upsampledInput[0][i] > 0){
 					upsampledInput[0][i] = 0.99;
 				}else{
@@ -54,10 +54,8 @@ Clipper::Clipper(IDetector *newUserInput):
 		{
 	settings = std::vector<Setting>{
 		Setting("gain", userInput->getInputHandler(ControllerInput::pot2), 5, 1, 50),
-		Setting("threshold", userInput->getInputHandler(ControllerInput::pot3), 3, 1, 10),
-		Setting("bias", userInput->getInputHandler(ControllerInput::pot4), 0, -10, 10),
-		Setting("character", userInput->getInputHandler(ControllerInput::pot6), 3, 1, 10),
-		Setting("mix", userInput->getInputHandler(ControllerInput::pot7), 20, 1, 20)
+		Setting("bias", userInput->getInputHandler(ControllerInput::pot3), 0, 0, 10),
+		Setting("mix", userInput->getInputHandler(ControllerInput::pot4), 20, 1, 20)
 	};
 }
 
